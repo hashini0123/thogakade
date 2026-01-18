5b0ad662fc63293ff8b25aea7404bb90f597cc45
hashini0123 <nanayakkarahashini5@gmail.com>
2026-01-18 22:38:33 +0530

    first commit

---
+++ src/main/java/controller/LoginInfoFormController.java
@@ -0,0 +1,9 @@
+package controller;
+
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginInfoFormController {
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+}
