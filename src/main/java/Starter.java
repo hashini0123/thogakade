44a2189cc8014d050eca7c1f67aae4f385f08d0f
hashini0123 <nanayakkarahashini5@gmail.com>
2026-01-18 22:26:50 +0530

    first commit

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Starter {
+public class Starter extends Application {
+    public static void main(String[] args){
+        launch();
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
+        stage.show();
+        stage.setTitle("Login Info");
+    }
 }
