965a6a3c6792a6e35a459c5f7dbf7aad86f74bc4
hashini0123 <nanayakkarahashini5@gmail.com>
2026-01-26 15:54:45 +0530

    created order file commit this application

--- src/main/java/controller/OrderDetailsInfoFormController.java
+++ src/main/java/controller/OrderDetailsInfoFormController.java
@@ -1,4 +1,113 @@
 package controller;
 
-public class OrderDetailsInfoFormController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.dto.OrderDetailsInfoDTO;
+
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class OrderDetailsInfoFormController implements Initializable {
+    ObservableList<OrderDetailsInfoDTO> orderDetailsInfoDTOS = FXCollections.observableArrayList();
+
+    @FXML
+    private TableColumn<?, ?> colDiscount;
+
+    @FXML
+    private TableColumn<?, ?> colItemCode;
+
+    @FXML
+    private TableColumn<?, ?> colOrderID;
+
+    @FXML
+    private TableColumn<?, ?> colOrderQty;
+
+    @FXML
+    private TableView<OrderDetailsInfoDTO> tblOrderDetailsInfo;
+
+    @FXML
+    private TextField txtDiscount;
+
+    @FXML
+    private TextField txtItemCode;
+
+    @FXML
+    private TextField txtOrderID;
+
+    @FXML
+    private TextField txtOrderQty;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        colOrderID.setCellValueFactory(new PropertyValueFactory<>("orderID"));
+        colItemCode.setCellValueFactory(new PropertyValueFactory<>("itemCode"));
+        colOrderQty.setCellValueFactory(new PropertyValueFactory<>("orderQty"));
+        colDiscount.setCellValueFactory(new PropertyValueFactory<>("discount"));
+
+        tblOrderDetailsInfo.setItems(orderDetailsInfoDTOS);
+
+        tblOrderDetailsInfo.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+            if (newValue != null){
+                txtOrderID.setText(newValue.getOrderID());
+                txtItemCode.setText(newValue.getItemCode());
+                txtOrderQty.setText(String.valueOf(newValue.getOrderQty()));
+                txtDiscount.setText(String.valueOf(newValue.getDiscount()));
+            }
+
+            loadTableOrderDetails();
+
+                });
+
+    }
+
+    public void loadTableOrderDetails() throws SQLException {
+        orderDetailsInfoDTOS.clear();
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade","root","1234");
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM orderdetail");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                orderDetailsInfoDTOS.add(new OrderDetailsInfoDTO(
+
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+
 }
--- src/main/java/model/dto/OrderDetailsInfoDTO.java
+++ src/main/java/model/dto/OrderDetailsInfoDTO.java
@@ -1,4 +1,20 @@
 package model.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class OrderDetailsInfoDTO {
+
+    private String orderID;
+    private String itemCode;
+    private Integer orderQty;
+    private Integer discount;
+
 }
